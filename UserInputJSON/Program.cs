00057ed830a4debb52b3a9731189508ffcb2ed7e
Kıraç Armağan Önal <kiracarmaganonal@gmail.com>
2021-01-31 15:20:38 +0300

    komke

--- UserInputJSON/Program.cs
+++ UserInputJSON/Program.cs
@@ -7,30 +7,29 @@ namespace UserInputJSON
     {
         static void Main(string[] args)
         {
-            InputManager inputManager = new InputManager();
+
+            InputManager inputManager = new InputManager(true, true);
 
             inputManager.OnKeyboardEvent += OnKeyboardEvent;
             inputManager.OnMouseEvent += OnMouseEvent;
 
-            inputManager.Initialize();
             Console.ReadLine();
         }
 
         static void OnMouseEvent(VirtualKeyCode key, KeyState state, int x, int y)
         {
-            if (key == VirtualKeyCode.Invalid)
-            {
-                Console.WriteLine(@"{{""event"": ""mouseMove"", ""xPosition"": {0}, ""yPosition"": {1}}}", x.ToString(), y.ToString());
-            } else
-            {
-                Console.WriteLine(@"{{""event"": ""mouseStateChange"", ""keyName"": ""{0}"", ""keyCode"": {1}, ""keyState"": ""{2}"", ""keyStateCode"": {3}, ""xPosition"": {4}, ""yPosition"": {5}}}", key.ToString(), ((int)key).ToString(), state.ToString(), ((int)state).ToString(), x.ToString(), y.ToString());
-            }
-            
+            string keyCode = ((int)key).ToString();
+            string xString = x.ToString();
+            string yString = y.ToString();
+
+            Console.WriteLine(@"{{""event"":{{""base"":""mouse"",""type"":""{3}""}},""data"":{{""x"":{0},""y"":{1},""key"":{2}}}}}", xString, yString, keyCode, (state == KeyState.Down ? "keydown" : (state == KeyState.Up ? "keyup" : (state == KeyState.Pressed ? "keypress" : "move"))));
         }
 
         static void OnKeyboardEvent(VirtualKeyCode key, KeyState state)
         {
-            Console.WriteLine(@"{{""event"": ""keyboardStateChange"", ""keyName"": ""{0}"", ""keyCode"": {1}, ""keyState"": ""{2}"", ""keyStateCode"": {3}}}", key.ToString(), ((int)key).ToString(), state.ToString(), ((int)state).ToString());
+            string keyCode = ((int)key).ToString();
+
+            Console.WriteLine(@"{{""event"":{{""base"":""keyboard"",""type"":""{1}""}},""key"":{0}}}", keyCode, (state == KeyState.Down ? "keydown" : (state == KeyState.Up ? "keyup" : (state == KeyState.Pressed ? "keypress" : ""))));
         }
     }
 }
